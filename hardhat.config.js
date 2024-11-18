04cb6ec9529dbf2ba3d9e2e2e89f1b7fb9ebd0d0
Sarvesh Gupta <gsarvesh387@gmail.com>
2024-11-18 17:59:49 +0530

    j

---
+++ hardhat.config.js
@@ -0,0 +1,28 @@
+require('@nomicfoundation/hardhat-toolbox')
+
+module.exports = {
+  defaultNetwork: 'localhost',
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: 'http://127.0.0.1:8545',
+    },
+    bitfinity: {
+      url: 'https://testnet.bitfinity.network',
+      accounts: ['a79b3b4140e8c629c90a859eaca26c4caee3e624547290c6187cbfce9dd65081'],
+      chainId: 355113,
+    },
+  },
+  solidity: {
+    version: '0.8.17',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  mocha: {
+    timeout: 40000,
+  },
+}
